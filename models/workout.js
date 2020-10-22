49d75e0978b792fa2d562b93afeed51447b9251e
Calvin Griffin <cgriffin332@gmail.com>
2020-10-22 15:38:56 -0400

    fixed workoutschema

--- models/workout.js
+++ models/workout.js
@@ -4,26 +4,30 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
   day: {
     type: Date,
-    default: new Date().setDate(new Date().getDate())
+    default: new Date().setDate(new Date().getDate()),
   },
-  type: {
-    type: String,
-    trim: true,
-    required: "Type is required.",
-  },
-  name: {
-    type: String,
-    trim: true,
-    required: "Name is required.",
-  },
-  duration: {
-    type: Number,
-    required: true,
-  },
-  distance: Number,
-  weight: Number,
-  reps: Number,
-  sets: Number,
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Type is required.",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Name is required.",
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      distance: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number,
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
