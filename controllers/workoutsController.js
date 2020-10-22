da12c795955e52309bd62aa7c47eefb85534795c
Calvin Griffin <cgriffin332@gmail.com>
2020-10-21 23:23:15 -0400

    added find one, edit, and create routes

--- controllers/workoutsController.js
+++ controllers/workoutsController.js
@@ -6,19 +6,62 @@ const db = require("../models");
 
 // find all workouts route
 router.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-      .then((foundWorkouts) => {
-        res.json(foundWorkouts);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.json({
-          error: true,
-          data: null,
-          message: "Failed to retrieve workouts.",
-        });
+  db.Workout.find({})
+    .then((foundWorkouts) => {
+      res.json(foundWorkouts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to retrieve workouts.",
       });
-  });
-
-
+    });
+});
+//find one workout route
+router.get("/api/workouts/:id", (req, res) => {
+  db.Workout.findById(req.params.id)
+    .then((foundWorkout) => {
+      res.json(foundWorkout);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: `Failed to retrieve workout with id: ${req.params.id}`,
+      });
+    });
+});
+// add one workout route
+router.get("/api/workouts", (req, res) => {
+  db.Workout.create(req.body)
+    .then((newWorkout) => {
+      res.json(newWorkout);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to create new workout.",
+      });
+    });
+});
+// update one workout route
+router.get("/api/workouts/:id", (req, res) => {
+  db.Workout.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((updatedWorkout) => {
+      res.json(updatedWorkout);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        error: true,
+        data: null,
+        message: "Failed to update workout.",
+      });
+    });
+});
 module.exports = router;
