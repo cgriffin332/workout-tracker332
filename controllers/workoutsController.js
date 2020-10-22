6e2a90ffaa281f6269a0bf3500e07b95b7b017cd
Calvin Griffin <cgriffin332@gmail.com>
2020-10-21 23:17:28 -0400

    added find all workouts route

--- controllers/workoutsController.js
+++ controllers/workoutsController.js
@@ -4,7 +4,21 @@ const router = express.Router();
 
 const db = require("../models");
 
-
+// find all workouts route
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then((foundWorkouts) => {
+        res.json(foundWorkouts);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: true,
+          data: null,
+          message: "Failed to retrieve workouts.",
+        });
+      });
+  });
 
 
 module.exports = router;
