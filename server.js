bcd5f591205e1dabe29ef56f589cc7bf33f5c58b
Calvin Griffin <cgriffin332@gmail.com>
2020-10-22 22:03:15 -0400

    ran prettier

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const logger = require("morgan")
+const logger = require("morgan");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 const workoutsController = require("./controllers/workoutsController");
 
-
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/workout",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 const connection = mongoose.connection;
 
@@ -41,7 +37,6 @@ app.get("/api/config", (req, res) => {
 
 app.use(workoutsController);
 require("./controllers/htmlRoutes")(app);
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
